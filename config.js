a12952c231db0458031ebf763611307b1d8df015
banditosjar <37003728+banditosjar@users.noreply.github.com>
2026-08-24 21:12:04 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -4,5 +4,6 @@
 // WICHTIG: Niemals den service_role Key in diese Datei eintragen.
 window.FIT4US_CONFIG = {
   supabaseUrl: "https://iuvhmickimdjpwkfxosv.supabase.co",
-  supabaseKey: "sb_publishable_B00lSqn8SF2vDPaUCPLjow_Zmgnx7JS"
+  supabaseKey: "sb_publishable_B00lSqn8SF2vDPaUCPLjow_Zmgnx7JS",
+  pushVapidPublicKey: "BLj7cMk4O7vbSm8rTv0OBdkZvSwtw77ZOv6VbaoY_XUTUUjcr6IvBcOLo8ix00Rvsjxp4xkraAd6SbO26kSdG6g"
 };
